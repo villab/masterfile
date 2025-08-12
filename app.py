68d85faf1a9d05977966bd212fef17b9dc6d40a5
villab <cevillate@gmail.com>
2025-08-12 14:57:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ from office365.sharepoint.client_context import ClientContext
 from office365.runtime.auth.user_credential import UserCredential
 
 # ================== CONFIGURACIÓN ==================
-USERNAME = st.secrets["sharepoint_user"]  # Tu usuario de Office 365
-PASSWORD = st.secrets["sharepoint_pass"]  # Tu contraseña o App Password
-SITE_URL = "https://caseonit.sharepoint.com/sites/Sutel"
 
+USERNAME = st.secrets["sharepoint_user"]
+APP_PASSWORD = st.secrets["app_password"]
+
+SITE_URL = "https://caseonit.sharepoint.com/sites/Sutel"
 # Ruta exacta del archivo en SharePoint (server-relative URL)
 FILE_URL = "/sites/Sutel/Documentos compartidos/01. Documentos MedUX/Automatizacion/MasterfileSutel.xlsx"
 
 # ================== CONEXIÓN A SHAREPOINT ==================
 try:
-    ctx = ClientContext(SITE_URL).with_credentials(UserCredential(USERNAME, PASSWORD))
+    ctx = ClientContext(SITE_URL).with_credentials(UserCredential(USERNAME, APP_PASSWORD))
     file = ctx.web.get_file_by_server_relative_url(FILE_URL)
     file_stream = BytesIO()
     file.download(file_stream).execute_query()
@@ -27,3 +28,4 @@ try:
 
 except Exception as e:
     st.error(f"Error al descargar el archivo: {e}")
+
