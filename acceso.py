be8378bbe6bff6456624539f1e855a498b99ec7b
villab <cevillate@gmail.com>
2025-08-12 14:29:41 -0500

    Update acceso.py

--- acceso.py
+++ acceso.py
@@ -1,16 +1,15 @@
-import streamlit as st
 from office365.sharepoint.client_context import ClientContext
 from office365.runtime.auth.user_credential import UserCredential
+import streamlit as st
 
 USERNAME = st.secrets["sharepoint_user"]
 APP_PASSWORD = st.secrets["app_password"]
+
 SITE_URL = "https://caseonit.sharepoint.com/sites/Sutel"
 
 try:
     ctx = ClientContext(SITE_URL).with_credentials(UserCredential(USERNAME, APP_PASSWORD))
-    web = ctx.web
-    ctx.load(web)
-    ctx.execute_query()
-    st.success(f"✅ Conectado correctamente a: {web.properties['Title']}")
+    web = ctx.web.get().execute_query()
+    st.write(f"Conectado a: {web.properties['Title']}")
 except Exception as e:
-    st.error(f"❌ Error de conexión: {e}")
+    st.error(f"Error de conexión: {e}")
